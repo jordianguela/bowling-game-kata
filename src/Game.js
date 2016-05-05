fa3d259d0867d043ae5ad49f43e5aa4957c88a5f
Jordi Anguela <jordi.anguela@gmail.com>
2016-05-05 23:00:03 +0200

    final score is 0 when all rolls are missed

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,11 @@
 function Game() {
 };
 
+Game.prototype.roll = function (pins) {
+};
+
+Game.prototype.score = function () {
+  return 0;
+};
+
 module.exports = Game;
--- test/game.spec.js
+++ test/game.spec.js
@@ -2,9 +2,14 @@ var Game = require('../src/Game');
 
 describe("Bowling Game Kata", function() {
 
-  it("result is 0 when all rolls are missed", function() {
+  it("final score is 0 when all rolls are missed", function() {
     var game = new Game();
 
+    for (i = 0; i < 20; i++) { 
+      game.roll(0);
+    }
+
+    expect(0).toBe(game.score());
   });
 
 });
